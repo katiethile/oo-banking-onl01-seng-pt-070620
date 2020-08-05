2dd38ca1321b9341c26d6268be0c8ea191e62888
Katie Le <lethikatie@gmail.com>
2020-08-05 04:53:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
 
-end
+  def deposit(deposit_money)
+    @balance += deposit_money
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end 
+  
+  def close_account
+    @status = 'closed'
+  end 
+  
+  def valid?
+    status == 'open' && balance > 0
+  end
+end 
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount = 50 )
+    @sender = sender 
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+    @transfer_count = 0
+  end 
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction
+    if sender.valid? && receiver.valid? && transfer_count < 1
+    self.status = 'complete'
+  else
+    self.status = 'rejected'
+    "Transaction rejected. Please check your account balance."
+  end 
+end
 end
